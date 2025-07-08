2597f1f26a14fc5b2347d2ebf458a0d0d8551718
khalidhossain <mdkhalidhossain5000@gmail.com>
2025-07-08 18:15:33 +0600

    article details api and view count api added

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-const app=express()
+const app = express();
 dotenv.config();
 const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r4vhlna.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -20,7 +19,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,16 +27,16 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     //DB AND COLLECTION STARTS
-    const db=client.db("Assignment_12_DB")
-    const articleCollections=db.collection('Articles')
+    const db = client.db("Assignment_12_DB");
+    const articleCollections = db.collection("Articles");
     //DB AND COLLECTION ENDS
 
     //article(submitted by user) related api starts (PRIVATE_API)
-    app.post('/articles',async (req,res)=>{
-      const articles=req.body;
-      const result=await articleCollections.insertOne(articles)
-      res.send(result)
-    })
+    app.post("/articles", async (req, res) => {
+      const articles = req.body;
+      const result = await articleCollections.insertOne(articles);
+      res.send(result);
+    });
 
     // Trending API Route
     app.get("/articles/trending", async (req, res) => {
@@ -54,15 +53,37 @@ async function run() {
         res.status(500).json({ message: "Internal server error" });
       }
     });
+    //get article details page single data
+    app.get("/articles/:id", async (req, res) => {
+      const id=req.params.id
+      
+      const article = await articleCollections.findOne({ _id: new ObjectId(id) });
+      res.send(article);
+    });
+
+    //ARTICLE VIEW COUNT-->
+    app.patch("/articles/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const result = await articleCollections.updateOne(
+          { _id: new ObjectId(id) },
+          { $inc: { views: 1 } }
+        );
+        if (result.modifiedCount === 0)
+          return res.status(404).json({ error: "Article not found" });
+        res.json({ message: "View count incremented" });
+      } catch (err) {
+        res.status(500).json({ error: "Failed to update views" });
+      }
+    });
+
     //article(submitted by user) related api ends
-
-
-
-
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -70,13 +91,12 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 // Default  route
-app.get('/', (req, res) => {
-    res.send('NewsPaper Server is running');
+app.get("/", (req, res) => {
+  res.send("NewsPaper Server is running");
 });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+  console.log(`Server is listening on port ${port}`);
 });
